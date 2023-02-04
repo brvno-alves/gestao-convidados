9fe55c2d1e5f90bbd507d053e257ba53de913ea0
Bruno R C Alves <bruno-rca@hotmail.com>
2023-02-04 00:43:24 -0300

    Listando objetos com Thymeleaf; Enviando um objeto do Controller para a View

--- src/main/java/com/bruno/gestaoconvidados/controller/ConvidadosController.java
+++ src/main/java/com/bruno/gestaoconvidados/controller/ConvidadosController.java
@@ -3,20 +3,29 @@ package com.bruno.gestaoconvidados.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import com.bruno.gestaoconvidados.model.Convidado;
 import com.bruno.gestaoconvidados.repository.Convidados;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
 	@Autowired
 	private Convidados convidados;
 	
-	@GetMapping("/convidados")
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 		modelAndView.addObject("convidados", convidados.findAll());
 		return modelAndView;
+	}
+		
+	@PostMapping("/convidados")
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
 	}
 }
